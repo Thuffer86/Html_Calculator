4ec74eae50f302a1c126079c712d40ea0627d26f
Thuffer86 <Thuffer86@gmail.com>
2015-03-21 11:00:35 -0700

    Changes to html and removed .js needs debugged

--- App.js
+++ App.js
@@ -1,2 +1,16 @@
-var a = Number(document.getElementById("1").value);
-console.log(a);
+$(document).ready(function() {
+$(".number,.symbol").click(function(){
+var v = $(this).val();
+$('#answer').val($('#answer').val() + v);
+});
+$('#clear').click(function(){
+    $('#answer').val('');
+    $('#operation').val('');
+    $('#operation').removeClass('activeAnswer');
+    $('#equals').attr('onclick','');
+});
+    $('.equals').click(function(){
+    var c =$('#answer').val();
+    $('#answer').val(eval(c));
+    });
+});
